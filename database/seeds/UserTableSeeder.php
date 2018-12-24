da1c60e02a15e1a4974754151c2a219fd27eb7a0
Zuam Xness <apkssky@gmail.com>
2018-12-24 16:10:17 +0700

    Using the query builder to make seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserTableSeeder extends Seeder
@@ -12,10 +13,12 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        App\User::create([
-	        'username'  => str_random(20),
-	        'email' => str_random(10) . '@demo.com',
-	        'password'  => Hash::make('secret')
-		]);
+        DB::table('users')->insert([
+            'username' => 'user1',
+            'email' => 'user1@email.com',
+            'password' => Hash::make('secret'),
+            'photo' => url('images/avatar-df.jpg'),
+            'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+        ]);
     }
 }
