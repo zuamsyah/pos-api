de5cd1420880ce81864411b9d4f90ddccfc5e7be
Zuam Xness <apkssky@gmail.com>
2018-12-26 14:12:10 +0700

    change with auth Customer

--- app/Customer.php
+++ app/Customer.php
@@ -9,11 +9,11 @@ class Customer extends Model
     protected $primaryKey = 'customer_id';
 
     protected $fillable = [
-      'name', 'address', 'phone_number', 'city_id'
+      'name', 'address', 'phone_number', 'city_id', 'user_id'
     ];
 
     public function user(){
-      return $this->belongsTo('App\User', 'customer_id');
+      return $this->belongsTo('App\User', 'user_id');
     }
 
     public function cities(){
