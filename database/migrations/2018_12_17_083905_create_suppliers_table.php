98ab1775183f7b8ae8c8b1e6cd37bb513801ab9d
Zuam Xness <apkssky@gmail.com>
2018-12-26 14:23:49 +0700

    add columns for relations

--- database/migrations/2018_12_17_083905_create_suppliers_table.php
+++ database/migrations/2018_12_17_083905_create_suppliers_table.php
@@ -15,11 +15,13 @@ class CreateSuppliersTable extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->increments('supplier_id');
-            $table->integer('city_id')->unsigned()->index();
+            $table->unsignedInteger('user_id')->index();
+            $table->unsignedInteger('city_id')->index();
             $table->string('name');
             $table->mediumText('address');
             $table->string('phone_number')->unique()->nullable();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('city_id')->references('id')->on('cities');
         });
     }
