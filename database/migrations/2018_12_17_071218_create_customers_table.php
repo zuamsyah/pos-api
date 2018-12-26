6a2abdb4963202b0ff3f2d10646abdc77a5ccb64
Zuam Xness <apkssky@gmail.com>
2018-12-26 14:23:26 +0700

    add columns for relations

--- database/migrations/2018_12_17_071218_create_customers_table.php
+++ database/migrations/2018_12_17_071218_create_customers_table.php
@@ -15,11 +15,13 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->increments('customer_id');
-            $table->integer('city_id')->unsigned()->index();
+            $table->unsignedInteger('user_id')->index();
+            $table->unsignedInteger('city_id')->index();
             $table->string('name');
             $table->mediumText('address');
             $table->string('phone_number')->unique()->nullable();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('city_id')->references('id')->on('cities');
         });
     }
