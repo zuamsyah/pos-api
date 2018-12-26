8431ec4e2bc204da031d755dcac55a07b5ec5f7e
Zuam Xness <apkssky@gmail.com>
2018-12-26 14:21:01 +0700

    add relation with user

--- app/Supplier.php
+++ app/Supplier.php
@@ -8,8 +8,12 @@ class Supplier extends Model
 {
 	protected $primaryKey = 'supplier_id';
     protected $fillable = [
-    	'name', 'address', 'phone_number', 'city_id'
+    	'name', 'address', 'phone_number', 'city_id', 'user_id'
     ];
+    
+    public function user(){
+        return $this->belongsTo('App\User', 'user_id');
+    }
 
     public function order(){
     	return $this->hasMany('App\Order', 'supplier_id');
