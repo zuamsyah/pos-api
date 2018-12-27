e83f3272ef1d46150b34bdc8d0ef3765c5b715b8
Zuam Xness <apkssky@gmail.com>
2018-12-27 17:17:13 +0700

    Add columns for relations

--- database/migrations/2018_12_17_090445_create_order_details_table.php
+++ database/migrations/2018_12_17_090445_create_order_details_table.php
@@ -17,10 +17,12 @@ class CreateOrderDetailsTable extends Migration
             $table->increments('id');
             $table->unsignedInteger('order_id')->index();
             $table->string('product_code',30)->index();
+            $table->unsignedInteger('user_id')->index();
             $table->bigInteger('product_amount');
             $table->bigInteger('buy_price');
             $table->string('subtotal_price')->default(0);
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('product_code')->references('product_code')->on('products')->onDelete('cascade');
             $table->foreign('order_id')->references('order_id')->on('orders')->onDelete('cascade');
         });
